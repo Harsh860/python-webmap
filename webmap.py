540728c6921e41e144a02315ace3b4837d5c1a34
Harsh860 <harshvardhan860@gmail.com>
2018-03-12 01:45:35 +0530

    added pointmarker for different population level

--- webmap.py
+++ webmap.py
@@ -7,10 +7,19 @@ lat=content["lat"]
 lon=content["lng"]
 population=content["pop"]
 
+def checkcolor(d):
+    if d > 500000 :
+        return "red"
+    elif d>=100000 :
+
+        return  "yellow"
+    else :
+        return "green"
+
 featuregroup=folium.FeatureGroup(name="population of cities")
 i=0
 for a,b,c,d in zip(city,lat,lon,population):
-    featuregroup.add_child(folium.CircleMarker(location=[b,c],radius = 1,popup=str(d),color='red'))
+    featuregroup.add_child(folium.CircleMarker(location=[b,c],radius = 1,popup=str(d),color=checkcolor(d)))
     
 
 map1 = folium.Map(zoom_start=10, tiles="Mapbox Bright")
